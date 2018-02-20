317dee619a44762ca6e1137a5d3d8f7b517df37e
lincolnmroth <lincoln@lroth.tech>
2018-02-19 21:33:10 -0500

    fixes broken code

--- slashcommands.py
+++ slashcommands.py
@@ -45,27 +45,27 @@ def submit():
         abort(400)
 
     return 'To submit your hack go to our devpost page, go.hackPHS.tech/submit'
-#
-# @app.route('/organizers', methods=['POST'])
-# def organizers():
-#     """Parse the command parameters, validate them, and respond.
-#     Note: This URL must support HTTPS and serve a valid SSL certificate.
-#     """
-#     # Parse the parameters you need
-#     token = request.form.get('token', None)  # TODO: validate the token
-#     command = request.form.get('command', None)
-#     text = request.form.get('text', None)
-#     # Validate the request parameters
-#     if not token:  # or some other failure condition
-#         abort(400)
-#
-#     with urllib.request.urlopen("https://hackprincetonhs.github.io/hackPHS-2018/admins.json") as url:
-#         data = json.loads(url.read().decode())
-#         organizers = "Organizers: \n"
-#         for i in data["events"]:
-#             organizers += i["name"]
-#             # schedule += ": "
-#             # schedule += i["title"]
-#             schedule += "\n"
-#         print(organizers)
-#         return organizers
+
+@app.route('/organizers', methods=['POST'])
+def organizers():
+    """Parse the command parameters, validate them, and respond.
+    Note: This URL must support HTTPS and serve a valid SSL certificate.
+    """
+    # Parse the parameters you need
+    token = request.form.get('token', None)  # TODO: validate the token
+    command = request.form.get('command', None)
+    text = request.form.get('text', None)
+    # Validate the request parameters
+    if not token:  # or some other failure condition
+        abort(400)
+
+    with urllib.request.urlopen("https://hackprincetonhs.github.io/hackPHS-2018/admins.json") as url:
+        data = json.loads(url.read().decode())
+        organizers = "Organizers: \n"
+        for i in data["events"]:
+            organizers += i["name"]
+            # schedule += ": "
+            # schedule += i["title"]
+            organizers += "\n"
+        print(organizers)
+        return organizers
